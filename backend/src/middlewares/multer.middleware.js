cc65d50772a2b99ec7d0e4bdd90ec51062175844
Vedant Games <guptavedant2549@gmail.com>
2024-05-30 02:09:18 +0530

    backend commit

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,6 +1,30 @@
 const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary').v2;
 
-const multerUploadImg = multer({ dest: '../../public/temp/imgs' });
-const multerUploadVid = multer({ dest: '../../public/temp/videos' });
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const imagesStorage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'images',
+    allowedFormats: ['jpg', 'png', 'jpeg']
+  }
+});
+
+const videosStorage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'videos',
+    allowedFormats: ['mp4', 'ogg', 'wav']
+  }
+});
+
+const multerUploadImg = multer({ storage: imagesStorage });
+const multerUploadVid = multer({ storage: videosStorage });
 console.log('me bol rha tha yahi par h');
 module.exports = { multerUploadImg, multerUploadVid };
