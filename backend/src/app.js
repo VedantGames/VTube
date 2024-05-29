70af46c01f9faa75138ee2e6bde0edd4244d6e7b
Vedant Games <guptavedant2549@gmail.com>
2024-05-30 01:38:11 +0530

    backend commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,18 +2,22 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-
+console.log('created app');
 app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true
 }))
 
-app.use(express.json());
+console.log('origin set');
 
+app.use(express.json());
+console.log('creating router');
 const userRouter = require('./routes/user.route');
 const videoRouter = require('./routes/video.route');
 
+console.log('assigning router');
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/videos', videoRouter);
 
+console.log('exporting router');
 module.exports = app;
